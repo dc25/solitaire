7a37160a83f89c5746e218dd78b2b952b4307daa
Dave Compton <davecompton7@gmail.com>
2015-03-15 17:52:33 -0700

    adding haskell top level for haste

---
+++ Solitare.hs
@@ -0,0 +1,33 @@
+import Haste
+import Haste.DOM
+import Haste.Events
+import Haste.Prim
+import Haste.Foreign
+
+import Data.Char
+import Control.Monad
+import Shuffle
+import Card
+import Game
+
+foreign import ccall loadCards :: Ptr (IO ()) -> IO ()
+foreign import ccall placeCard :: JSString -> Int -> Int -> IO ()
+
+loadCallback = do
+    placeCard (toJSStr "1_club") 0 0 
+    placeCard (toJSStr "king_diamond") 400 50
+    placeCard (toJSStr "3_heart") 300 100
+    placeCard (toJSStr "black_joker") 200 150
+    placeCard (toJSStr "jack_spade") 340 250
+
+main = do 
+          shuffledDeck <- shuffle [ Card r s | r<-[Ace .. King], s<-[Hearts .. Clubs]] 
+
+          let
+              foundations' = replicate 4 []
+              columns' =     replicate 7 $ Column [] []
+              deck' =       []
+              game = Game foundations' columns' deck' shuffledDeck
+              gameInPlay = start game
+
+          loadCards(toPtr loadCallback)

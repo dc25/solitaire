10842f10357727ca0a954e1541e569411f0353fd
Dave Compton <davecompton7@gmail.com>
2015-03-16 05:16:05 -0700

    changed higher bound and added clarifying comment

--- Shuffle.hs
+++ Shuffle.hs
@@ -4,13 +4,25 @@ module Shuffle
 
 import Haste.App 
 
+-- Note that randomR has higher bound of "length ys'-1"
+-- According to the documentation, the higher bound is
+-- "exclusive".  If this is true then the higher bound
+-- should be "length ys'" .  However, setting the bound 
+-- to "length ys'" causes bad runtime behavior so I am
+-- guessing that the bound is "inclusive".  
+--
+-- How can I verify that this is the case other than
+-- just observing the result.  Would like to be able
+-- to do an assert().
+--
+
 shuffle :: [a] -> IO [a]
 shuffle ys = do
     gen <- newSeed 
     return $ shuffle' gen ys where
         shuffle' _ [] = []
         shuffle' gen ys' = 
-            let (r, newGen) = randomR (0, length ys' ) gen
+            let (r, newGen) = randomR (0, length ys'-1 ) gen -- see comment
                 (a,b) = splitAt r ys'
             in head b : shuffle' newGen (a ++ tail b)
 

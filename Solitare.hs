5fe9cb6e77bddb0fe7d7cf95664feb35d34cb743
Dave Compton <davecompton7@gmail.com>
2015-03-16 05:17:11 -0700

    adding some display code

--- Solitare.hs
+++ Solitare.hs
@@ -13,6 +13,29 @@ import Game
 foreign import ccall loadCards :: Ptr (IO ()) -> IO ()
 foreign import ccall placeCard :: JSString -> Int -> Int -> IO ()
 
+rankSVGString :: Rank -> String
+rankSVGString Ten =   "10"
+rankSVGString Jack =  "jack"
+rankSVGString Queen = "queen"
+rankSVGString King =  "king"
+rankSVGString r = [chr ((fromEnum r - fromEnum Ace) + ord '1')]
+
+suitSVGString :: Suit -> String
+suitSVGString Hearts =   "heart"
+suitSVGString Diamonds = "diamond"
+suitSVGString Spades =   "spade"
+suitSVGString Clubs =    "club"
+
+svgString (Card rank suit) = rankSVGString rank ++ "_" ++ suitSVGString suit
+
+showDeck :: [Card] -> IO ()
+showDeck deck = 
+           sequence_ (map pc $ zip [ (x,y) | x <- [0..70], y <- [0..5] ] deck)
+               where pc ((x,y), card) = placeCard (toJSStr $ svgString card) (100*x) (120*y)
+
+showGame :: Game -> IO ()
+showGame game = return ()
+
 loadCallback = do
     shuffledDeck <- shuffle [ Card r s | r<-[Ace .. King], s<-[Hearts .. Clubs]] 
     let
@@ -21,12 +44,8 @@ loadCallback = do
         deck' =       []
         game = Game foundations' columns' deck' shuffledDeck
         gameInPlay = start game
-
-    placeCard (toJSStr "1_club") 0 0 
-    placeCard (toJSStr "king_diamond") 400 50
-    placeCard (toJSStr "3_heart") 300 100
-    placeCard (toJSStr "black_joker") 200 150
-    placeCard (toJSStr "jack_spade") 340 250
+    showDeck $ shuffledDeck
+    return () -- why is this necessary?
 
 main = do 
           loadCards(toPtr loadCallback)

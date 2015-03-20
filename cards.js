ec5be1cf147fa9cdd8c5efd1fcd71dc62b5410ed
Dave Compton <davecompton7@gmail.com>
2015-03-19 21:01:15 -0700

    regenerated .js files

--- cards.js
+++ cards.js
@@ -9,13 +9,22 @@ var B;
 function showAlert_ffi(msg) {
     alert(msg);
 }
+// For debugging.
+function consoleLog_ffi(msg) {
+    console.log(msg);
+}
 // Global scale to apply to all cards displayed
 var cardScale = 0.5;
 var drag = d3.behavior.drag().on("dragstart", dragstart).on("drag", dragmove).on("dragend", dragend);
 function dragstart() {
     d3.event.sourceEvent.stopPropagation();
+    var selectArg;
     // Turn off mouseover across the board while dragging
-    var selectArg = 'g[class*="visible"]';
+    selectArg = 'g[class*="visible"]';
+    d3.selectAll(selectArg).on("mouseover", null);
+    selectArg = 'g[class="solitareDeck"]';
+    d3.selectAll(selectArg).on("mouseover", null);
+    selectArg = 'g[class="hiddenReserves"]';
     d3.selectAll(selectArg).on("mouseover", null);
 }
 // Define drag beavior
@@ -32,15 +41,21 @@ function setDragEndCallback_ffi(cb) {
 }
 // Define dragend behavior - just call back into haskell.
 function dragend(d) {
+    var selectArg;
     // Turn on mouseover for all visible objects when done dragging
-    var selectArg = 'g[class*="visible"]';
+    selectArg = 'g[class*="visible"]';
     d3.selectAll(selectArg).on("mouseover", mouseover);
-    // additional select("g") because card is nested below dragged object
-    var draggedId = d3.select(this).select("g").attr("id");
+    selectArg = 'g[class="solitareDeck"]';
+    d3.selectAll(selectArg).on("mouseover", mouseover);
+    selectArg = 'g[class="hiddenReserves"]';
+    d3.selectAll(selectArg).on("mouseover", mouseover);
+    var dragged = d3.select(this);
+    var draggedClassName = dragged.attr("class");
+    var draggedId = dragged.select("g").attr("id"); // select("g") because card is nested below dragged object
     var coordinates = d3.mouse(this.parentNode);
     var xCoord = coordinates[0];
     var yCoord = coordinates[1];
-    B(A(dragEndCallback, [[0, draggedId], [0, xCoord], [0, yCoord], 0]));
+    B(A(dragEndCallback, [[0, draggedId], [0, draggedClassName], [0, xCoord], [0, yCoord], 0]));
 }
 // Provide for callback into haskell when mouse passes over object
 var mouseoverCallback;
@@ -50,12 +65,13 @@ function setMouseoverCallback_ffi(cb) {
 }
 // Define mouseover behavior - just call back into haskell.
 function mouseover(d, i) {
-    // additional select("g") because card is nested below event object
-    var draggedId = d3.select(this).select("g").attr("id");
+    var moused = d3.select(this);
+    var mousedClassName = moused.attr("class");
+    var mousedId = moused.select("g").attr("id");
     var coordinates = d3.mouse(this.parentNode);
     var xCoord = coordinates[0];
     var yCoord = coordinates[1];
-    B(A(mouseoverCallback, [[0, draggedId], [0, xCoord], [0, yCoord], 0]));
+    B(A(mouseoverCallback, [[0, mousedId], [0, mousedClassName], [0, xCoord], [0, yCoord], 0]));
 }
 function getBaseOffset(card) {
     // queryString thanks to : http://stackoverflow.com/questions/23034283/is-it-possible-to-use-htmls-queryselector-to-select-by-xlink-attribute-in-an
@@ -66,7 +82,10 @@ function getBaseOffset(card) {
     return { x: xOffset, y: yOffset };
 }
 function alignCard_ffi(name, classname, x, y) {
-    var card = document.getElementById(name);
+    // Thanks to : 
+    // http://stackoverflow.com/questions/10337640/how-to-access-the-dom-element-that-correlates-to-a-d3-svg-object
+    // for telling how to use node() to retrieve DOM element from selection.
+    var card = d3.select('body svg g[data-name="' + name + '"]').node();
     var baseOffset = getBaseOffset(card);
     d3.select('body svg g[data-name="' + name + '"]').data([{ xtranslate: (0 + x / cardScale - baseOffset.x), ytranslate: (235.27 + y / cardScale - baseOffset.y) }]).attr("class", function (d, i) {
         return classname;
@@ -74,8 +93,8 @@ function alignCard_ffi(name, classname, x, y) {
         return "scale (" + cardScale + ")" + "translate (" + d.xtranslate + "," + d.ytranslate + ")";
     });
 }
-function placeCard_ffi(name, classname, x, y) {
-    var card = document.getElementById(name);
+function placeCard_ffi(id, name, classname, x, y) {
+    var card = document.getElementById(id);
     var baseOffset = getBaseOffset(card);
     d3.select("body svg").append("g").each(function (d, i) {
         this.appendChild(card.cloneNode(true));
@@ -88,9 +107,9 @@ function placeCard_ffi(name, classname, x, y) {
     }).on("mouseover", mouseover);
     // There must be a better way of enabling drag 
     // for new cards in a visble column.
-    if (classname.indexOf("visibleColumn") > -1) {
+    if ((classname.indexOf("visibleColumn") > -1) || (classname == "hiddenReserves") || (classname == "solitareDeck")) {
         var selectArg = "g[class=" + classname + "]";
-        d3.select(selectArg).call(drag);
+        d3.selectAll(selectArg).call(drag);
     }
 }
 function deleteBySelectionString_ffi(cssSelection) {
